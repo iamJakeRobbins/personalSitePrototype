85ae3e56d5df4a6b5770467fdfb31bbe11a7e1e1
Jake Robbins <jrobbinsd@gmail.com>
2019-01-05 14:01:06 -0700

    header template for testing/outlining

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,17 @@
+<template >
+	<div class="headerContainer">
+		<h1>This is the page Header</h1>
+	</div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+.headerContainer{
+	border-bottom: 1px solid black
+}
+</style>
