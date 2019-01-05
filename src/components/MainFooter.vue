51ecf8feb6d8f36e8d01217faa1d20adfbf9cd3c
Jake Robbins <jrobbinsd@gmail.com>
2019-01-05 14:00:43 -0700

    footer template for testing/outlining

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="footerContainer">
+  	<h3>This is the page footer</h3>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+.footerContainer{
+	border-top: 1px solid black
+}
+</style>
