c26bfd4596eb0b7ffb36e8f732b0436668dfbebb
Jake Robbins <jrobbinsd@gmail.com>
2019-01-05 14:53:00 -0700

    roughed out link selection for page header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,6 +1,13 @@
 <template >
 	<div class="headerContainer">
 		<h1>This is the page Header</h1>
+		<h3>collection of links to:</h3>
+		<ul>
+			<li>tech </li>
+			<li>websites (sad)</li>
+			<li>resume/contact</li>
+			<li>about</li>
+		</ul>
 	</div>
 
 </template>
