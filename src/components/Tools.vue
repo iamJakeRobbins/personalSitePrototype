b213b87bf6f1663cfa7e8c2434d0cdab7f3628c4
Jake Robbins <jrobbinsd@gmail.com>
2019-01-06 12:53:51 -0700

    component init

---
+++ src/components/Tools.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+	<div class="scrollContainer">
+			<h2>we're going to test a scrolling banner here!</h2>
+	</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
