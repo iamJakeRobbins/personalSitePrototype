b302082e1985427c46c0b188bd1462038122638f
Jake Robbins <jrobbinsd@gmail.com>
2019-01-06 13:05:24 -0700

    added images for test

--- src/components/Tools.vue
+++ src/components/Tools.vue
@@ -1,6 +1,21 @@
 <template lang="html">
 	<div class="scrollContainer">
 			<h2>we're going to test a scrolling banner here!</h2>
+			<div id="container">
+    <!-- Each image is 350px by 233px -->
+    <div class="photobanner">
+    	<img class="first" src="image-1.jpg" alt="" />
+    	<img src="../assets/images/4k-wallpaper-alpine-alps-1484728.jpg" alt="" />
+    	<img src="image-3.jpg" alt="" />
+    	<img src="image-4.jpg" alt="" />
+    	<img src="image-5.jpg" alt="" />
+    	<img src="image-6.jpg" alt="" />
+    	<img src="image-1.jpg" alt="" />
+    	<img src="image-2.jpg" alt="" />
+    	<img src="image-3.jpg" alt="" />
+    	<img src="image-4.jpg" alt="" />
+    </div>
+</div>
 	</div>
 </template>
 
