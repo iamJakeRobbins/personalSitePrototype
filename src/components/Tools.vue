89b60d243e4bc8ef27aacd8ab9dc0bc8f10cdac0
Jake Robbins <jrobbinsd@gmail.com>
2019-01-06 17:32:53 -0700

    cleaned up code, need to add spacing and then work on adding hover text

--- src/components/Tools.vue
+++ src/components/Tools.vue
@@ -1,27 +1,19 @@
 <template lang="html">
 	<div class="scrollContainer">
-			<h2>we're going to test a scrolling banner here!</h2>
-			<div id="container">
-    <header>
-     <h1>Animated Photo Banner</h1>
-     <p>Lorem ipsum dolor...</p>
-    </header>
-    <!-- Each image is 350px by 233px -->
-		<transition>
-    <div class="photobanner">
-    	<img class="first" src="../assets/images/moonlit.jpg" alt="" />
-    	<img src="../assets/images/alpine_alps.jpg" alt="" />
-    	<img src="../assets/images/cabins.jpg" alt="" />
-    	<img src="../assets/images/fingal_head.jpg" alt="" />
-    	<img src="../assets/images/foggy.jpg" alt="" />
-    	<img src="../assets/images/moon_mountain.jpg" alt="" />
-			<img src="../assets/images/moonlit.jpg" alt="" />
-			<img src="../assets/images/alpine_alps.jpg" alt="" />
-    	<img src="../assets/images/cabins.jpg" alt="" />
-    	<img src="../assets/images/fingal_head.jpg" alt="" />
-    </div>
-		</transition>
-</div>
+		<div id="container">
+	    <div class="photobanner">
+	    	<img class="first" src="../assets/images/moonlit.jpg" alt="" />
+	    	<img src="../assets/images/alpine_alps.jpg" alt="" />
+	    	<img src="../assets/images/cabins.jpg" alt="" />
+	    	<img src="../assets/images/fingal_head.jpg" alt="" />
+	    	<img src="../assets/images/foggy.jpg" alt="" />
+	    	<img src="../assets/images/moon_mountain.jpg" alt="" />
+				<img src="../assets/images/moonlit.jpg" alt="" />
+				<img src="../assets/images/alpine_alps.jpg" alt="" />
+	    	<img src="../assets/images/cabins.jpg" alt="" />
+	    	<img src="../assets/images/fingal_head.jpg" alt="" />
+	    </div>
+		</div>
 	</div>
 </template>
 
